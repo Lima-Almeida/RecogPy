997d397f038497dc7ead62372da875bb2b1b5daa
Lima-Almeida <m242827@dac.unicamp.br>
2025-11-10 20:16:36 -0300

    implemented fingers states and hand inclination

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # RecogPy
-Facial/Gesture recognition with python
+Facial/Gesture recognition with python.
+
+At the current state, this application uses webcam live footage to recognize your hand(s).
+
+Output -> [x, x, x, x, x] xx.xx
+
+The vector "[x, x, x, x, x]" represents the states of your 5 fingers, being [thumb, index, middle, ring, pinky]. The state 1 represents a stretched finger and the state 0 represents a bent/flexed finger.
+
+The number "xx.xx" given after the vector represents the angle of your hand.
--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import math
 
 
 cap = cv2.VideoCapture(0)
@@ -14,6 +15,48 @@ hands = mp_hands.Hands(max_num_hands=2,
                        min_detection_confidence=0.7,
                        min_tracking_confidence=0.7)
 
+def hand_inclination(hand_landmarks):
+    degrees = None
+
+    midfinger_base_id = 9
+    wrist_id = 0
+
+    midfinger_base = hand_landmarks.landmark[midfinger_base_id]
+    wrist = hand_landmarks.landmark[wrist_id]
+
+    y_len = wrist.y - midfinger_base.y #y-axis is inverted
+    x_len = midfinger_base.x - wrist.x
+
+    rad = math.atan2(x_len, y_len)
+    degrees = math.degrees(rad)
+
+    return degrees
+
+def finger_states(hand_landmarks):
+    finger_states = []
+    fingertips_ids = [8, 12, 16, 20] #excluding thumb
+    bases_ids = [6, 10, 14, 18] #excluding thumb
+    thumb_tip_id = 4
+    thumb_base_id = 2
+
+    thumb_tip = hand_landmarks.landmark[thumb_tip_id]
+    thumb_base = hand_landmarks.landmark[thumb_base_id]
+
+    if thumb_tip.x < thumb_base.x:
+        finger_states.append(1)
+    else:
+        finger_states.append(0)
+
+    for k in range(len(fingertips_ids)):
+        tip = hand_landmarks.landmark[fingertips_ids[k]]
+        base = hand_landmarks.landmark[bases_ids[k]]
+        if tip.y < base.y:
+            finger_states.append(1)
+        else:
+            finger_states.append(0)
+
+    return finger_states
+
 
 while True:
     ret, frame = cap.read()
@@ -26,7 +69,6 @@ while True:
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     results = hands.process(rgb)
-    print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
@@ -35,6 +77,8 @@ while True:
                 hand_landmarks,
                 mp_hands.HAND_CONNECTIONS
             )
+
+            print(finger_states(hand_landmarks), "{:.2f}".format(hand_inclination(hand_landmarks)))
     
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
